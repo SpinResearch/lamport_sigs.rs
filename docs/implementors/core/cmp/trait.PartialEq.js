c4b7f7ec885c0d18b00538b6fbc5d37d5de975a1
Frederic Jacobs <github@fredericjacobs.com>
2016-11-11 13:35:45 +0100

    Adding developer documentation.

---
+++ docs/implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["lamport"] = ["impl&lt;T:&nbsp;Digest + <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='lamport/struct.PrivateKey.html' title='lamport::PrivateKey'>PrivateKey</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()

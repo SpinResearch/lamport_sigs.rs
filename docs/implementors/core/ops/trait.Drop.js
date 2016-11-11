31f3a4df5948f9e198d921bd900031ed6bcda7db
Frederic Jacobs <github@fredericjacobs.com>
2016-11-11 13:35:45 +0100

    Adding developer documentation.

---
+++ docs/implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["lamport"] = ["impl&lt;T:&nbsp;Digest + <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='lamport/struct.PrivateKey.html' title='lamport::PrivateKey'>PrivateKey</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
